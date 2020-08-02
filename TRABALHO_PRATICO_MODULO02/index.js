84bdb56355e97428e2d18dfcc32d31ce790f8dd7
Douglas Vallinhos] <douglas.m.vallinhos@gmail.com>
2020-08-02 20:04:18 -0300

    adicionado comentarios para facilitar a leitura

--- TRABALHO_PRATICO_MODULO02/index.js
+++ TRABALHO_PRATICO_MODULO02/index.js
@@ -3,16 +3,19 @@ import { promises as fs } from 'fs';
 init();
 
 async function init() {
-  //await createFiles();
-  //await citiesCount('SP').then((a) => console.log(a));
-  //await fiveMoreCities();
-  //await fiveLessCities();
-  //await nameMoreCities();
-  //await nameLessCities();
+  await createFiles();
+  await citiesCount('SP').then((a) => console.log(a));
+  await fiveMoreCities();
+  await fiveLessCities();
+  await nameMoreCities();
+  await nameLessCities();
   await allNameMoreCities();
   await allNameLessCities();
 }
-
+/* Criar uma função que irá criar um arquivo JSON para cada estado representado no
+arquivo Estados.json, e o seu conteúdo será um array das cidades pertencentes a
+aquele estado, de acordo com o arquivo Cidades.json. O nome do arquivo deve ser
+o UF do estado, por exemplo: MG.json. */
 async function createFiles() {
   let cities = JSON.parse(await fs.readFile('./files/Cidades.json'));
   let states = JSON.parse(await fs.readFile('./files/Estados.json'));
@@ -25,13 +28,17 @@ async function createFiles() {
     );
   }
 }
-
+/* Criar uma função que recebe como parâmetro o UF do estado, realize a leitura do
+arquivo JSON correspondente e retorne a quantidade de cidades daquele estado. */
 async function citiesCount(state) {
   const city = await fs.readFile(`./states/${state}.json`);
   const cityJson = JSON.parse(city);
   return cityJson.length;
 }
-
+/* Criar um método que imprima no console um array com o UF dos cinco estados
+que mais possuem cidades, seguidos da quantidade, em ordem decrescente. Você
+pode usar a função criada no tópico 2. Exemplo de impressão: [“UF - 93”, “UF - 82”,
+“UF - 74”, “UF - 72”, “UF - 65”] */
 async function fiveMoreCities() {
   const states = JSON.parse(await fs.readFile('./files/Estados.json'));
   const listStates = [];
@@ -57,7 +64,10 @@ async function fiveMoreCities() {
 
   console.log(listFiveFinal);
 }
-
+/* Criar um método que imprima no console um array com o UF dos cinco estados
+que menos possuem cidades, seguidos da quantidade, em ordem decrescente.
+Você pode usar a função criada no tópico 2. Exemplo de impressão: [“UF - 30”, “UF
+- 27”, “UF - 25”, “UF - 23”, “UF - 21”] */
 async function fiveLessCities() {
   const states = JSON.parse(await fs.readFile('./files/Estados.json'));
   const listStates = [];
@@ -84,7 +94,9 @@ async function fiveLessCities() {
 
   console.log(listFiveFinal);
 }
-
+/* Criar um método que imprima no console um array com a cidade de maior nome de
+cada estado, seguida de seu UF. Por exemplo: [“Nome da Cidade – UF”, “Nome da
+Cidade – UF”, ...]. */
 async function nameMoreCities() {
   const states = JSON.parse(await fs.readFile('./files/Estados.json'));
   const citiesMoreLenght = [];
@@ -105,7 +117,9 @@ async function nameMoreCities() {
 
   console.log(citiesMoreLenght);
 }
-
+/* Criar um método que imprima no console um array com a cidade de menor nome
+de cada estado, seguida de seu UF. Por exemplo: [“Nome da Cidade – UF”, “Nome
+da Cidade – UF”, ...]. */
 async function nameLessCities() {
   const states = JSON.parse(await fs.readFile('./files/Estados.json'));
   const citiesMoreLenght = [];
@@ -126,7 +140,8 @@ async function nameLessCities() {
 
   console.log(citiesMoreLenght);
 }
-
+/* Criar um método que imprima no console a cidade de maior nome entre todos os
+estados, seguido do seu UF. Exemplo: “Nome da Cidade - UF". */
 async function allNameMoreCities() {
   const states = JSON.parse(await fs.readFile('./files/Estados.json'));
   const citiesMoreLenght = [];
@@ -151,7 +166,8 @@ async function allNameMoreCities() {
 
   console.log(citiesMoreLenght[0]);
 }
-
+/* Criar um método que imprima no console a cidade de menor nome entre todos os
+estados, seguido do seu UF. Exemplo: “Nome da Cidade - UF". */
 async function allNameLessCities() {
   const states = JSON.parse(await fs.readFile('./files/Estados.json'));
   const citiesMoreLenght = [];
